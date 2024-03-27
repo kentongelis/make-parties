b9bacb4735ad83be2a62b8c24fc414d3fcbd7e3e
kentongelis <kenton.gelis@students.dominican.edu>
2024-03-26 20:34:54 -0700

    users can destroy events

--- app.js
+++ app.js
@@ -70,6 +70,15 @@ app.put('/events/:id', (req, res) => {
     });
 });
 
+// DELETE
+app.delete('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id).then(event => {
+      event.destroy();
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
